6280a97704f0fbf70b18e2f80b678a2834d7771a
Mr-Sena <jsenna985@gmail.com>
2022-01-19 00:44:16 -0300

    Transfêrencia Representacional dos dados
     - Form/input -> Padrão de projeto para recepção das informações
     - DTO/output -> Padrão de projeto para exposição dos dados

--- Workspace/src/main/java/br/com/alura/challenge/backend/FinanceValuation/core/domain/controller/ReceitaController.java
+++ Workspace/src/main/java/br/com/alura/challenge/backend/FinanceValuation/core/domain/controller/ReceitaController.java
@@ -1,18 +1,22 @@
 package br.com.alura.challenge.backend.FinanceValuation.core.domain.controller;
 
 
+import br.com.alura.challenge.backend.FinanceValuation.core.domain.controller.DTO.ReceitaDTO;
+import br.com.alura.challenge.backend.FinanceValuation.core.domain.controller.form.ReceitaForm;
 import br.com.alura.challenge.backend.FinanceValuation.core.domain.model.Receita;
 import br.com.alura.challenge.backend.FinanceValuation.infrastructure.repository.ReceitaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/receita")
+@RequestMapping("/receitas")
 public class ReceitaController {
 
 
@@ -22,13 +26,24 @@ public class ReceitaController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<Receita> novaReceita(@RequestBody Receita modelo) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(receitaRepository.save(modelo));
+    public ResponseEntity<ReceitaDTO> novaReceita(@RequestBody ReceitaForm formulario, UriComponentsBuilder uriBuilder) {
+
+        Receita receita = formulario.toReceita();
+        receitaRepository.save(receita);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(receita.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ReceitaDTO(receita));
+
+        //return ResponseEntity.status(HttpStatus.CREATED).body(receitaRepository.save(formulario));
+
     }
 
     @GetMapping
-    public List<Receita> buscarTodos() {
-        return receitaRepository.findAll();
+    public List<ReceitaDTO> buscarTodos() {
+
+        List<Receita> receitas = receitaRepository.findAll();
+        return ReceitaDTO.toReceitaDTO(receitas);
+
     }
 
 
